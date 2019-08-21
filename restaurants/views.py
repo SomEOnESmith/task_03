6eb2b4c7ed6434b44b833b6cb8b4793f51996fd2
Huusain_AlHaddad <h.ali.alhaddad@hotmail.com>
2019-08-22 01:27:52 +0430

    finish task 3

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,11 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    	'my_list': [
+    	{"restaurant_name": "China Town" , "food_type": "chinese"},
+    	{"restaurant_name": "elevation burger" , "food_type": "burgers"},
+    	{"restaurant_name": "papa jones" , "food_type": "pizzas"},
+    	],
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +18,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    	"my_object": {
+    		"restaurant_name": "China Town" ,
+    		 "food_type": "chinese",
+    	}
 
     }
     return render(request, 'detail.html', context)
